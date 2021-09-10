2c8250492d883d250ba4244e8cc7b73171946a0f
leanor13 <leanor@yandex.ru>
2021-09-10 10:56:31 +0300

    added option to add sequences through admin

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,12 @@
+# pylint: disable=missing-module-docstring
 from django.contrib import admin
+from .models import Sequence
 
-# Register your models here.
+class StorageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'sequence')
+#    list_display_links = ('id', 'title')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Sequence, StorageAdmin)
