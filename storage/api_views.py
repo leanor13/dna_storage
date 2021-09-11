fbae4ed41f2b9adcff68e1a3bad9676f9a5d53ff
leanor13 <leanor@yandex.ru>
2021-09-11 19:51:12 +0300

    New sequence POST returns JSON now

--- storage/api_views.py
+++ storage/api_views.py
@@ -2,6 +2,7 @@ from django.http.response import JsonResponse
 from rest_framework.generics import ListAPIView,CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from storage.serializers import SequenceSerializer
 from storage.models import Sequence
@@ -9,13 +10,17 @@ from storage.models import Sequence
 class SequenceList(ListAPIView):
     queryset = Sequence.objects.all()
     serializer_class = SequenceSerializer
-
-class SequenceCreate (CreateAPIView):
+ 
+class SequenceCreate(CreateAPIView):
     serializer_class = SequenceSerializer
 
     def create(self, request, *args, **kwargs):
         sequence = request.data.get('sequence')
         for n in sequence:
             if n not in ['A','C','T','G','a','c','t','g']:
-                raise ValidationError({'sequence': 'Should contain only ACTG'})
-        return super().create(request, *args, **kwargs)
+                return Response({'status': 'Failed', 'sequence': 'Should contain only ACTG'})
+        super().create(request, *args, **kwargs)
+        return Response({'status':'Ok', 'message':'sequence uploaded successfully'})
+
+    
+
