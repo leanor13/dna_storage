fed685b84a5101b4dfc6a02c90197be2d12b9293
leanor13 <leanor@yandex.ru>
2021-09-15 13:22:53 +0300

    changed API to /seq/ with CRUD functions

--- storage/views.py
+++ storage/views.py
@@ -9,3 +9,17 @@ from storage.models import Sequence
 class SequenceViewSet(viewsets.ModelViewSet):
     queryset = Sequence.objects.all()
     serializer_class = SequenceSerializer
+
+    def create(self, request, *args, **kwargs):
+        '''
+        Can be reached by link http://localhost:8000/seq
+        Validations: 
+            both fields should be mandatory, 
+            'sequence' should have only ACTGactg symbols
+        '''
+        sequence = request.data.get('sequence')
+        for n in sequence:
+            if n not in ['A','C','T','G','a','c','t','g']:
+                return Response({'status': 'Failed', 'message': 'Sequence should contain only ACTG'})
+        super().create(request, *args, **kwargs)
+        return Response({'status':'Ok', 'message':'Sequence uploaded successfully'})
